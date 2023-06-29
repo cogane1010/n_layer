9ea34ce658f30a2e9b40f54a0e33efd72c80fd3b
Nguyễn Hoàng <hoang.nt@brggroup.vn>
2023-06-29 14:31:41 +0700

    update package

--- App.Core/Helper/ExternalApiHelper.cs
+++ App.Core/Helper/ExternalApiHelper.cs
@@ -8,24 +8,24 @@ namespace App.Core.Helper
 {
     public class ExternalApiHelper
     {
-       public static TResult PostBodyJson<TBody, TResult>(string url, Dictionary<string, string> headerParams, TBody body, int timeout = -1)
-            where TBody : class
-            where TResult : class
-        {
-            var client = new RestClient(url);
-            client.Timeout = timeout;
-            var request = new RestRequest(Method.POST);
-            foreach (KeyValuePair<string, string> p in headerParams)
-            {
-                request.AddHeader(p.Key, p.Value);
-            }
+       //public static TResult PostBodyJson<TBody, TResult>(string url, Dictionary<string, string> headerParams, TBody body, int timeout = -1)
+       //     where TBody : class
+       //     where TResult : class
+       // {
+       //     var client = new RestClient(url);
+       //     client.Timeout = timeout;
+       //     var request = new RestRequest(Method.POST);
+       //     foreach (KeyValuePair<string, string> p in headerParams)
+       //     {
+       //         request.AddHeader(p.Key, p.Value);
+       //     }
 
-            var bodyString = JsonConvert.SerializeObject(body);
-            request.AddParameter("application/json", bodyString, ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
-            var result = JsonConvert.DeserializeObject<TResult>(response.Content);
-            return result;
+       //     var bodyString = JsonConvert.SerializeObject(body);
+       //     request.AddParameter("application/json", bodyString, ParameterType.RequestBody);
+       //     IRestResponse response = client.Execute(request);
+       //     var result = JsonConvert.DeserializeObject<TResult>(response.Content);
+       //     return result;
 
-        }
+       // }
     }
 }
